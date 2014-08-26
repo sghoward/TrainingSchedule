ed668947f31f9c2f3dcc1485f8946fd1ef55302b
sghoward <s.graham.howard@outlook.com>
2014-08-25 22:02:54 -0400

    Added in blank views for client along with a blank version of the clientcontroller

---
+++ TrainingSchedule/Controllers/ClientController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrainingSchedule.Controllers
+{
+    public class ClientController : Controller
+    {
+        //
+        // GET: /Client/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Event()
+        {
+            return View();
+        }
+
+        public ActionResult UpcomingEvent()
+        {
+            return View();
+        }
+
+        public List<Event> getAllEvents()
+        {
+            List<Event> results = new List<Event>();
+            return results;
+
+        }
+
+        public Event getEvent(int eventID)
+        {
+            //use db context to get a single event
+            return event;
+        }
+
+
+    }
+}
